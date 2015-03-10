7b67d80eeb419883ff6bd2729385763ce8f77d58
elliot <postsneakernet@gmail.com>
2015-03-10 19:53:09 -0400

    Add test cases for cosine similarity

--- docsim/core/tests.py
+++ docsim/core/tests.py
@@ -11,3 +11,18 @@ class UtilsTest(TestCase):
 
     def test_can_stem_words(self):
         self.fail('Implement this test')
+
+
+class CosineTest(TestCase):
+
+    def test_term_freq_finds_each_word_count(self):
+        self.fail('Implement this test')
+
+    def test_inverse_doc_freq_finds_idf(self):
+        self.fail('Implement this test')
+
+    def test_can_calculate_tf_idf(self):
+        self.fail('Implement this test')
+
+    def test_calc_cos_returns_cosinine_similarity(self):
+        self.fail('Implement this test')
