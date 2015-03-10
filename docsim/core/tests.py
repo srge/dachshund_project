4b1ebe58d49a2fd0539591cd5df8c572dc4207dc
Sergio Pais <sp@srge.org>
2015-03-10 19:29:50 -0400

    added 3 unit tests for utils.py

--- docsim/core/tests.py
+++ docsim/core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class UtilsTest(TestCase):
+
+    def test_can_tokenize(self):
+        self.fail('Implement this test')
+
+    def test_can_remove_stop_words(self):
+        self.fail('Implement this test')
+
+    def test_can_stem_words(self):
+        self.fail('Implement this test')
