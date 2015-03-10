c6bbbc4afd6303ba78eff3276b46707393ffaa86
elliot <postsneakernet@gmail.com>
2015-03-10 19:21:35 -0400

    Add initial comparisons tests

--- docsim/comparisons/tests.py
+++ docsim/comparisons/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ComparisonModelTest(TestCase):
+    def test_can_save_model(self):
+        self.fail('Implement this test')
+
+
+class HomePageTest(TestCase):
+    def test_home_page_renders_home_template(self):
+        self.fail('Implement this test')
+
+
+class ResultsPageTest(TestCase):
+    def test_results_page_renders_results_template(self):
+        self.fail('Implement this test')
