b650e8d670568613ff44d491aaf2a521b9dd131f
elliot <postsneakernet@gmail.com>
2015-03-12 21:55:21 -0400

    Add functional tests

--- docsim/functional_tests/tests.py
+++ docsim/functional_tests/tests.py
@@ -1,16 +1,41 @@
 from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import WebDriver
 
-class VisitorTest(LiveServerTestCase):
+class DocsimTests(LiveServerTestCase):
 
-	def setUp(self):
-		self.browser = webdriver.Firefox()
-		self.browser.implicitly_wait(3)
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-	def tearDown(self):
-		self.browser.quit()
+    def tearDown(self):
+        self.browser.quit()
 
-	def test_can_visit_home_page(self):
-		self.browser.get(self.live_server_url)
-		self.fail('implement test')
+    def test_home_page_renders(self):
+        self.fail('Implement this test')
+
+    def test_user_can_submit_form(self):
+        self.fail('Implement this test')
+
+    def test_user_cannot_submit_invalid_form(self):
+        self.fail('Implement this test')
+
+    def test_results_page_renders(self):
+        self.fail('Implement this test')
+
+    def test_results_page_contains_cosine(self):
+        self.fail('Implement this test')
+
+    def test_results_page_contains_hamming(self):
+        self.fail('Implement this test')
+
+    def test_results_page_contains_levenshtein(self):
+        self.fail('Implement this test')
+
+    def test_results_page_contains_sorensen(self):
+        self.fail('Implement this test')
+
+    def test_results_page_contains_jaccard(self):
+        self.fail('Implement this test')
+
+    def test_results_page_contains_consensus(self):
+        self.fail('Implement this test')
