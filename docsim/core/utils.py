52b9e76b1565ab2a1fe459fca1b33d1c32cb6d4e
Sergio Pais <sp@srge.org>
2015-03-17 15:48:17 -0400

    Add remove_stop_words function to utils

--- docsim/core/utils.py
+++ docsim/core/utils.py
@@ -4,3 +4,9 @@ def tokenize(test_string):
     words = nltk.word_tokenize(test_string)
 
     return words
+
+def remove_stop_words(test_list):
+    stopwords = nltk.corpus.stopwords.words('english')
+    list_result = [w for w in test_list if w not in stopwords]
+    return list_result
+
