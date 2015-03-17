7cc2f3e08d6f2506cf0fc87ecd9d1d48fb323ad6
elliot <postsneakernet@gmail.com>
2015-03-17 19:56:20 -0400

    Add tests and implentations for Jaccard and Sorensen

--- docsim/core/setsimilarity.py
+++ docsim/core/setsimilarity.py
@@ -10,26 +10,43 @@ class SetSimilarity(object):
     def build_union(self):
         raise NotImplementedError
 
+    def calculate_sim(self):
+        raise NotImplementedError
+
 
 class Sorensen(SetSimilarity):
 
     def __init__(self, list_a, list_b):
         SetSimilarity.__init__(self, list_a, list_b)
+        self.build_intersect()
+        self.build_union()
 
     def build_intersect(self):
-        return list(set(self.list_a) & set(self.list_b))
+        self.intersect = list(set(self.list_a) & set(self.list_b))
+        return self.intersect
 
     def build_union(self):
-        return list(set(self.list_a) | set(self.list_b))
+        self.union = list(set(self.list_a) | set(self.list_b))
+        return self.union
+
+    def calculate_sim(self):
+        return 2 * len(self.intersect) / len(self.union)
 
 
 class Jaccard(SetSimilarity):
-    
+
     def __init__(self, list_a, list_b):
         SetSimilarity.__init__(self, list_a, list_b)
+        self.build_intersect()
+        self.build_union()
 
     def build_intersect(self):
-        return list(set(self.list_a) & set(self.list_b))
+        self.intersect = list(set(self.list_a) & set(self.list_b))
+        return self.intersect
 
     def build_union(self):
-        return list(set(self.list_a) | set(self.list_b))
+        self.union = list(set(self.list_a) | set(self.list_b))
+        return self.union
+
+    def calculate_sim(self):
+        return len(self.intersect) / len(self.union)
