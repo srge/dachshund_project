751a66278ab7034cca4ed538dbc7a106bb0855ba
Sergio Pais <sp@srge.org>
2015-03-17 15:59:10 -0400

    change utils function parameters to not have test_ prefix

--- docsim/core/utils.py
+++ docsim/core/utils.py
@@ -2,20 +2,20 @@ from nltk import word_tokenize
 from nltk.stem.lancaster import LancasterStemmer
 from nltk.corpus import stopwords
 
-def tokenize(test_string):
-    return word_tokenize(test_string)
+def tokenize(text):
+    return word_tokenize(text)
 
 
-def stem_words(test_list):
+def stem_words(list_words):
     stem = LancasterStemmer().stem
     list_stem = []
 
-    for word in test_list:
+    for word in list_words:
         list_stem.append(stem(word))
 
     return list_stem
 
 
-def remove_stop_words(test_list):
-    list_result = [w for w in test_list if w not in stopwords.words('english')]
+def remove_stop_words(list_words):
+    list_result = [w for w in list_words if w not in stopwords.words('english')]
     return list_result
