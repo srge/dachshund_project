a8b43307716bed437eec989df72b3a51f6280491
elliot <postsneakernet@gmail.com>
2015-04-09 21:47:30 -0400

    Get correct view from url

--- docsim/docsim/urls.py
+++ docsim/docsim/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
 from docsim import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home'),
+    #url(r'^$', views.home, name='home'),
+    url(r'^$', include('comparisons.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
