b1f7fea235e57dfbd86b4b534a6133ee5e9fb464
elliot <postsneakernet@gmail.com>
2015-04-09 23:33:44 -0400

    Create consensus.py

---
+++ docsim/core/consensus.py
@@ -0,0 +1,11 @@
+from core import utils
+from core.cosine import Cosine
+from core.docdistance import Hamming
+from core.docdistance import Levenshtein
+from core.setsimilarity import Sorensen
+from core.setsimilarity import Jaccard
+
+class Consensus(object):
+
+    def __init__(self, doc_a, doc_b):
+        pass
