2da0fd34ad23bd027c6f66898dd26537b418b17e
elliot <postsneakernet@gmail.com>
2015-04-09 23:33:11 -0400

    Implement function tests

--- docsim/functional_tests/tests.py
+++ docsim/functional_tests/tests.py
@@ -15,28 +15,107 @@ class DocsimTests(LiveServerTestCase):
         self.assertIn('Dachshund', self.browser.title)
 
     def test_user_can_submit_form(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        self.assertIn('Results', self.browser.title)
 
     def test_user_cannot_submit_invalid_form(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        self.assertNotIn('Results', self.browser.title)
 
     def test_results_page_renders(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        self.assertIn('Results', self.browser.title)
 
     def test_results_page_contains_cosine(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        table = self.browser.find_element_by_id('id_results')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('Cosine:', [row.text for row in rows])
 
     def test_results_page_contains_hamming(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        table = self.browser.find_element_by_id('id_results')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('Hamming:', [row.text for row in rows])
 
     def test_results_page_contains_levenshtein(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        table = self.browser.find_element_by_id('id_results')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('Levenshtein:', [row.text for row in rows])
+
 
     def test_results_page_contains_sorensen(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        table = self.browser.find_element_by_id('id_results')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('Sorensen:', [row.text for row in rows])
 
     def test_results_page_contains_jaccard(self):
-        self.fail('Implement this test')
+        self.browser.get('http://localhost:8000')
+        doc_a = self.browser.find_element_by_id('id_doc_a')
+        doc_b = self.browser.find_element_by_id('id_doc_b')
+        doc_a.send_keys('Hello doc a')
+        doc_b.send_keys('Hello doc b')
+        button = self.browser.find_element_by_id('id_button')
+        button.click()
+
+        table = self.browser.find_element_by_id('id_results')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('Jaccard:', [row.text for row in rows])
 
     def test_results_page_contains_consensus(self):
         self.fail('Implement this test')
