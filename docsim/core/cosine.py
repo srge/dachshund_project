c7a14aa00319ac0f3814364ca6e9a7f9331bbbf0
elliot <postsneakernet@gmail.com>
2015-04-12 21:48:07 -0400

    Fix doc_ab using all words and not 200 most common

--- docsim/core/cosine.py
+++ docsim/core/cosine.py
@@ -13,18 +13,22 @@ class Cosine:
         self.stem_doc_b = utils.stem_words(self.stop_doc_b)
         self.term_freq_a = self.term_freq(self.stem_doc_a)
         self.term_freq_b = self.term_freq(self.stem_doc_b)
-        self.doc_ab = list(set(self.stem_doc_a) | set(self.stem_doc_b))
+        self.doc_ab = list(set(list(self.term_freq_a.keys())).union(
+                list(self.term_freq_b.keys())))
         self.inv_doc_freq = self.inverse_doc_freq(self.doc_ab,
-                                self.term_freq_a, self.term_freq_b)
+                self.term_freq_a, self.term_freq_b)
+        self.tf_idf_a = self.tf_idf(self.term_freq_a, self.inv_doc_freq)
+        self.tf_idf_b = self.tf_idf(self.term_freq_b, self.inv_doc_freq)
+        self.cos_result = self.calc_cos(self.tf_idf_a, self.tf_idf_b)
 
     def term_freq(self, list_words):
         fdist = nltk.FreqDist(list_words).most_common(200)
         return dict(fdist)
 
-    def inverse_doc_freq(self, list_words_ab, list_tf_a, list_tf_b):
+    def inverse_doc_freq(self, list_words_ab, tf_a, tf_b):
         dict_words = {}
         for word in list_words_ab:
-            if word in list_tf_a and word in list_tf_b:
+            if word in tf_a and word in tf_b:
                 dict_words[word] = math.log10(2/2)
             else:
                 dict_words[word] = math.log10(2/1)
@@ -32,8 +36,10 @@ class Cosine:
 
     def tf_idf(self, dict_tf, dict_idf):
         dict_tfidf = {}
-        for (k,v), (k2,v2) in zip(dict_tf.items(), dict_idf.items()):
-            if(k == k2):
-                tfidf = float(v*v2)
-                dict_tfidf[k] = tfidf
+
+        for k in dict_tf:
+            dict_tfidf[k] = dict_tf[k] * dict_idf[k]
         return dict_tfidf
+
+    def calc_cos(self, tf_idf_a, tf_idf_b):
+        pass
