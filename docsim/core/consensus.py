8536f6e53313819e1049ad4974d4d7425fd4112d
elliot <postsneakernet@gmail.com>
2015-04-14 18:50:46 -0400

    Add consensus.py

--- docsim/core/consensus.py
+++ docsim/core/consensus.py
@@ -8,4 +8,20 @@ from core.setsimilarity import Jaccard
 class Consensus(object):
 
     def __init__(self, doc_a, doc_b):
-        pass
+        self.list_a = utils.tokenize(doc_a)
+        self.list_b = utils.tokenize(doc_b)
+        self.jac = Jaccard(self.list_a, self.list_b).calculate_sim()
+        self.sor = Sorensen(self.list_a, self.list_b).calculate_sim()
+        self.ham = Hamming(self.list_a, self.list_b).calculate_dist()
+        self.lev = Levenshtein(self.list_a, self.list_b).calculate_dist()
+        self.cos = Cosine(doc_a, doc_b).cos_result
+
+    def get_consensus(self):
+        results = {
+                'jac': self.jac,
+                'sor': self.sor,
+                'ham': self.ham,
+                'lev': self.lev,
+                'cos': self.cos,
+            }
+        return results
