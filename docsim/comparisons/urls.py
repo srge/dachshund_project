f926e29a7744b47d73aac3823b737b0208c9f1c3
elliot <postsneakernet@gmail.com>
2015-04-14 18:49:32 -0400

    Implement get_results

--- docsim/comparisons/urls.py
+++ docsim/comparisons/urls.py
@@ -4,5 +4,6 @@ from comparisons import views
 urlpatterns = patterns(
     '',
     url(r'^results/', views.get_results, name='results'),
+    url(r'^results/(?P<id_c>[0-9]+)/$', views.get_results, name='results_id'),
     url(r'^$', views.home_page, name='home'),
 )
--- docsim/comparisons/views.py
+++ docsim/comparisons/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from comparisons.models import Comparison
+from core.consensus import Consensus
 
 
 def home_page(request):
@@ -10,9 +11,8 @@ def home_page(request):
 
         if comparison.doc_a and comparison.doc_b:
             comparison.save()
-            context = {'test':'test',}
 
-            return redirect('/results/')
+            return redirect(get_results, id_c=comparison.id)
 
         else:
             return redirect('/')
@@ -20,8 +20,20 @@ def home_page(request):
     return render(request, 'base.html')
 
 
-def get_results(request):
+def get_results(request, id_c=1):
     if not 'HTTP_REFERER' in request.META:
         return redirect('/')
 
-    return render(request, 'results.html')
+    comparison = Comparison.objects.order_by('-id')[0]
+    consensus = Consensus(comparison.doc_a, comparison.doc_b)
+    results = consensus.get_consensus()
+
+    return render(request, 'results.html', {
+            'doc_a': comparison.doc_a,
+            'doc_b': comparison.doc_b,
+            'jac': results['jac'],
+            'sor': results['sor'],
+            'ham': results['ham'],
+            'lev': results['lev'],
+            'cos': results['cos'],
+        })
